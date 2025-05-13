4db5db14d982ad3ec3841ad36c94a6cf26d2601a
aliyawl <aliyalaila2005@gmail.com>
2025-05-13 15:32:23 +0700

    Update register.php

--- PA PWD/pages/register.php
+++ PA PWD/pages/register.php
@@ -16,14 +16,17 @@
     <div class="login-container">
         <center><h1> Register</h1></center>
         <form action="../proses/cek-register.php" method="post">
-            <label for="nama" class="form-label">Username</label>
-                <input type="text" class="form-control" id="nama" placeholder="" required> <br>
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="" required><br>
 
-        <label for="namapassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="nama" placeholder="" required> <br>
-            <input class="btn btn-primary" type="submit" value="Register"> 
-            <br> <br> <h6>Already have an account? <a href="login.php">login</a> </h6>            
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="" required><br>
+
+            <input class="btn btn-primary" type="submit" value="Register"><br>
+            <h6>Already have an account? <a href="login.php">login</a></h6>
         </form>
+
+
     </div>
     
 
